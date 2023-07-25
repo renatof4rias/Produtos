7eb421ebefe3e9af75d995059f34af9f75fff46d
renatof4rias <renatof4rias@gmail.com>
2023-07-25 17:20:14 -0300

    Entidades e End points Criados

---
+++ backend/src/test/java/com/API/LOL/API4/LOL/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.LOL.API4.LOL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
